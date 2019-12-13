0c2d7c6680575ac6bbd9a4b53b290356aa1f07c3
fherbine <felix.herbinet@yahoo.com>
2019-12-13 19:28:32 +0100

    Install script done

--- install.py
+++ install.py
@@ -1,6 +1,25 @@
-#!/usr/bin/bash
+import os
+import getpass
 
-pip install -r requirements.txt
+if __name__ == '__main__':
+    os.system('pip install -r requirements.txt')
+    user = getpass.getuser()
+    home = os.path.join('/', 'home', user)
+    install_path = os.getcwd()
+    desktop_file = 'battery-notifier.desktop'
 
-cp battery-notifier.service /etc/systemd/system/
+    # following part could differ depending on your version
+    if not os.path.exists(os.path.join(home, '.config')):
+        os.mkdir(os.path.join(home, '.config'))
 
+    autostart_folder = os.path.join(home, '.config', 'autostart')
+
+    if not os.path.exists(autostart_folder):
+        os.mkdir(autostart_folder)
+    # --------
+
+    with open(desktop_file, 'a') as dfile:
+        dfile.write('Exec=%s/start.sh' % install_path)
+
+    os.system(f'cp {desktop_file} {autostart_folder}/')
+    print('Install done!\nYou can reboot your system NOW.')
